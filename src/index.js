9f7ffa96ceb9de2215b33d6367659b64acac46fb
강효선 <kanghyoseon@ganghyoseon-ui-MacBook-Pro.local>
2019-07-19 02:01:58 +0900

    [MOD]modify component

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import './style.css';
 import { DocumentSelectionExample } from './component/document-selection-example';
 import { excute } from './component/d3-force-layout-dag-component';
 
-// const excute = () => {
-//     const documentSelectionExample = new DocumentSelectionExample();
-// };
-
+// 실 데이터로 반영할 경우 두번째 인자를 지우면 됨.
 excute(parent.document, true);
