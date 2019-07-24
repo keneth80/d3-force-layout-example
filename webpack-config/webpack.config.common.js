eeaa6b0c5e72c5770e9770806d95c976c2ea5c78
hyoskang <pretty9967@naver.com>
2019-07-24 09:12:14 +0900

    [MOD]modify webpack config

--- webpack-config/webpack.config.common.js
+++ webpack-config/webpack.config.common.js
@@ -3,6 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebPackPlugin      = require('html-webpack-plugin');
 const BundleAnalyzerPlugin   = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
 
 const helpers = require('./helpers');
 
@@ -59,15 +60,30 @@ module.exports = {
             dry: false
         }),
 
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-            filename: './index.html'
+        new RemovePlugin({
+            before: {
+                include: [helpers.root('dist')]
+            },
+            after: {
+                test: [
+                    {
+                        folder: helpers.root('dist/component'),
+                        method: (filePath) => {
+                            return new RegExp(/\.map$/, 'm').test(filePath);
+                        }
+                    } 
+                ]
+            }
         }),
 
         new CopyWebpackPlugin([
             { from: './src/component/data/**', to: './component/data', flatten: true }
-        ])
+        ]),
 
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
         // new BundleAnalyzerPlugin()
     ]
 };
